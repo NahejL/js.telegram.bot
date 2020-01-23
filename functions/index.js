fb4b8d825082dab85c24caa8a33d7540c4b84590
NahejL <nahejlefebvre@gmail.com>
2020-01-23 02:47:12 -0500

    init \nfunctional http handler passing request to telegraf

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+const Telegraf = require('telegraf')
+
+admin.initializeApp()
+
+let botToken 
+{
+    admin.firestore().collection('root').doc('bot').get()
+    .then(snapshot => {
+        return botToken = snapshot.token
+    })
+    .catch(error => {
+        console.error(error)
+    })
+}
+
+let bot = new Telegraf(botToken)
+/*bot.on('text', (ctx) => {
+    ctx.reply('Hello World')
+})*/
+
+exports.bot = functions.https.onRequest((req, res) => {
+    bot.handleUpdate(req.body, res)
+})
